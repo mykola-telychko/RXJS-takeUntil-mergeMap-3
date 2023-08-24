5253cda853d7194ca0b1f3df628d83b1a8b0627e
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-24 18:06:20 +0300

    fin

--- index.ts
+++ index.ts
@@ -3,21 +3,24 @@ import './style.css';
 import { fromEvent } from 'rxjs';
 import { takeUntil, mergeMap, map } from 'rxjs/operators';
 
-const mousedown$ = fromEvent(document, 'mousedown')
+// https://www.learnrxjs.io/learn-rxjs/operators/filtering/takeuntil
+// Example 3: Take mouse events on mouse down until mouse up
+const mousedown$ = fromEvent(document, 'mousedown');
 const mouseup$ = fromEvent(document, 'mouseup');
 const mousemove$ = fromEvent(document, 'mousemove');
 
 // after mousedown, take position until mouse up
-mousedown$.pipe(
-  mergeMap(_ => {
-    return mousemove$.pipe(
-      map((e: any) => ({
-        x: e.clientX,
-        y: e.clientY
-      })),
-      // complete inner observable on mouseup event
-      takeUntil(mouseup$)
-    )
-  })
-)
-.subscribe(console.log);
+mousedown$
+  .pipe(
+    mergeMap((_) => {
+      return mousemove$.pipe(
+        map((e: any) => ({
+          x: e.clientX,
+          y: e.clientY,
+        })),
+        // complete inner observable on mouseup event
+        takeUntil(mouseup$)
+      );
+    })
+  )
+  .subscribe(console.log);
